d2b3e5c64ddd214db4e0c20f185e224f444b5601
kaguya0209 <k016c1308@it-neec.jp>
2019-05-24 07:38:41 +0900

    SQLite3追加

---
+++ Pasuke/Pasuke.iOS/SQLService.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+using Xamarin.Forms;
+[assembly: Dependency(typeof(SQLService))]
+public class SQLService : ISQLService
+{
+    public SQLiteConnection GetConnection()
+    {
+        var personalPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+        var libraryPath = System.IO.Path.Combine(personalPath, "..", "Library");
+        var path = System.IO.Path.Combine(libraryPath, "Pasukedb");
+        return new SQLiteConnection(path);
+    }
+}
