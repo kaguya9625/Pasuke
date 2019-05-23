8055e2a414c271e5d5d709d2d37cac7b3d6e2a24
kaguya0209 <k016c1308@it-neec.jp>
2019-05-24 07:37:53 +0900

    SQLite3追加

--- Pasuke/Pasuke.Android/MainActivity.cs
+++ Pasuke/Pasuke.Android/MainActivity.cs
@@ -18,6 +18,7 @@ namespace Pasuke.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App(new AndroidInitializer()));
+
         }
     }
 
---
+++ Pasuke/Pasuke.Android/SQLService.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLService))]
+public class SQLService : ISQLService
+{
+    public SQLiteConnection GetConnection()
+    {
+        var personalPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+        var path = System.IO.Path.Combine(personalPath, "Pasukedb");
+        return new SQLiteConnection(path);
+    }
+}
